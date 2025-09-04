cad81ab339f8dee2f0895c9c43d3406570b13a5e
cactus-beep-beep <cameronbak09@gmail.com>
2025-09-04 21:05:49 +0000

    final

--- CPA.java
+++ CPA.java
@@ -1,3 +1,19 @@
 public class CPA {
-  public static void main(string[]args) {
+  public static void main(String[]args){
+    int x = 10;
+    x *= 10;
+    x += 2;
+    x -= 2;
+    x = 1;
+    x *= 1;
+    x *= 3;
+    x *= 10;
+    x *= 5;
+    x += 5 ;
+    x -= 5;
+    x %= 10;
+
+    System.out.println(x);
+  }
+}
   
